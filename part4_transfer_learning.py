6674892e454f9c4b14bcd949ec4fa6941425fb4b
jan1na <antilope666@gmail.com>
2025-04-20 20:13:23 +0200

    adjust to use on laptop

--- part4_transfer_learning.py
+++ part4_transfer_learning.py
@@ -1,43 +1,67 @@
+import tensorflow
 import tensorflow as tf
 from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.datasets import cifar10
 from utils.eval import plot_confusion_matrix
 import visualkeras
 
-# Load CIFAR-10 dataset
+# TODO: Play around with hyperparameters
+TRAIN_SIZE = 10000
+TEST_SIZE = 2000
+BATCH_SIZE = 16
+IMAGE_SIZE = 96  # Input size for MobileNetV2
+DENSE_LAYER_SIZE = 128  # Size of the dense layer after the base model
+TRAIN_EPOCHS = 5  # Number of epochs for training
+
+
+# Load CIFAR-10 and reduce dataset size for speed on laptop
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+x_train, y_train = x_train[:TRAIN_SIZE], y_train[:TRAIN_SIZE]
+x_test, y_test = x_test[:TEST_SIZE], y_test[:TEST_SIZE]
 
-# Resize images to 96x96 to fit MobileNetV2 input
-x_train = tf.image.resize(x_train, (96, 96)) / 255.0
-x_test = tf.image.resize(x_test, (96, 96)) / 255.0
 
-y_train_cat = to_categorical(y_train, 10)
-y_test_cat = to_categorical(y_test, 10)
+# Preprocessing function for tf.data pipeline
+def preprocess(x: tf.Tensor, y: tf.Tensor) -> (tf.Tensor, tf.Tensor):
+    """
+    Preprocess the input data for the model. Resizes the images to 96x96 and normalizes them to [0, 1]. Converts labels
+    to one-hot encoding.
 
-# Use MobileNetV2 as a base model
-base_model = MobileNetV2(weights='imagenet', include_top=False, input_shape=(96, 96, 3))
+    :param x: Input image tensor.
+    :param y: Input label tensor.
+    """
+    x = tf.image.resize(x, (IMAGE_SIZE, IMAGE_SIZE)) / 255.0  # slightly smaller than 96x96
+    y = tf.one_hot(tf.squeeze(y), depth=10)
+    return x, y
+
+
+# Prepare datasets
+train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train)).map(preprocess).shuffle(5000).batch(BATCH_SIZE)
+test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).map(preprocess).batch(BATCH_SIZE)
+
+# Load MobileNetV2 without top layers
+base_model = MobileNetV2(weights='imagenet', include_top=False, input_shape=(IMAGE_SIZE, IMAGE_SIZE, 3))
 base_model.trainable = False  # Freeze the base model
 
 # Build the model
 x = base_model.output
 x = GlobalAveragePooling2D()(x)
-x = Dense(128, activation='relu')(x)  # TODO: Try other layer sizes
+x = Dense(DENSE_LAYER_SIZE, activation='relu')(x)
 predictions = Dense(10, activation='softmax')(x)
 
 model = Model(inputs=base_model.input, outputs=predictions)
 
-# Compile the model
-model.compile(optimizer=Adam(learning_rate=0.0001),  # TODO: Try changing the learning rate
-    loss='categorical_crossentropy', metrics=['accuracy'])
+# Compile model
+model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Visualize model architecture
 visualkeras.layered_view(model, legend=True, to_file='./plots/mobile_net_v2_model_architecture.png')
 
-# Train the model
-# TODO: Try different batch sizes or epochs
-model.fit(x_train, y_train_cat, validation_data=(x_test, y_test_cat), epochs=5, batch_size=64)
+# Train model
+model.fit(train_ds, validation_data=test_ds, epochs=TRAIN_EPOCHS)
 
-# Evaluate the model
-plot_confusion_matrix(model, x_test, y_test, 'Transfer Learning Model', './plots/confusion_matrix_tl.png')
+# Evaluate with confusion matrix
+x_test_resized = tf.image.resize(x_test, (IMAGE_SIZE, IMAGE_SIZE)) / 255.0
+plot_confusion_matrix(model, x_test_resized, y_test, 'Transfer Learning Model', './plots/confusion_matrix_tl.png')
